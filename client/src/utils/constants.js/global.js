d491f99878b83c1d3521b80f2eaeeb9ddbc77ab8
christinanikolova <christinanikolova@github.com>
2023-12-23 08:51:18 +0200

    reorder folders

---
+++ client/src/utils/constants.js/global.js
@@ -0,0 +1,56 @@
+export const emails = {
+    ADMIN: 'admin@pregnancydiary.com',
+  };
+  
+  // export const directions = {
+  //   PREV: 'prev',
+  //   NEXT: 'next',
+  // };
+  
+  // export const httpMethods = {
+  //   GET: 'GET',
+  //   POST: 'POST',
+  //   PUT: 'PUT',
+  //   DELETE: 'DELETE',
+  // };
+  
+  // export const formNames = {
+  //   CREATE: 'create',
+  //   UPDATE: 'update',
+  // };
+  
+  // export const classNames = {
+  //   SHOW: 'show',
+  //   HIDE: 'hide',
+  //   FORM_WIDTH: 'form-width',
+  //   CHEVRON_DOWN: 'fa-chevron-down',
+  //   CHEVRON_RIGHT: 'fa-chevron-right',
+  // };
+  
+  // export const styleNames = {
+  //   FLEX: 'flex',
+  //   NONE: 'none',
+  //   INLINE_BLOCK: 'inline-block',
+  //   BLOCK: 'block',
+  // };
+  
+  // export const genders = {
+  //   MALE: 'male',
+  //   FEMALE: 'female',
+  // };
+  
+  // export const addButtonTexts = {
+  //   SUB_TASK: 'sub-task',
+  //   TASK: 'task',
+  //   EVENT: 'event',
+  //   GUEST: 'guest',
+  //   NOTE: 'note',
+  //   COST: 'cost',
+  //   COMMENT: 'comment',
+  // };
+  
+  // export const tagNames = {
+  //   P: 'P',
+  //   H4: 'H4',
+  // };
+  
