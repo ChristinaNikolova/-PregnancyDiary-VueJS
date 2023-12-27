6551b8ab7388b01e6368787417df2886575fc1c5
christinanikolova <christinanikolova@github.com>
2023-12-27 10:13:34 +0200

    add dummy components

---
+++ client/src/components/administration/category/All.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>All Categories</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/shared/errors/ServerError.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>Server error</div>
+</template>
+
+<style scoped>
+
+</style>
