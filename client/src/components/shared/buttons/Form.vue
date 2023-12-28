23a7e98449e55c591cffb84888950796a5a216e0
christinanikolova <christinanikolova@github.com>
2023-12-28 09:52:01 +0200

    implement form's buttons

---
+++ client/src/components/shared/buttons/Form.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  formName: {
+    type: String,
+    default: 'Create',
+  },
+  isDisabled: {
+    type: Boolean,
+    default: true,
+  },
+});
+const emit = defineEmits(['onCancelFormHandler']);
+</script>
+
+<template>
+  <div class="form-buttons-wrapper">
+    <button :disabled="props.isDisabled" type="submit" class="btn btn-primary">
+      {{ props.formName }}
+    </button>
+    <button type="button" class="btn btn-secondary" @click="emit('onCancelFormHandler')">
+      Cancel
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.form-buttons-wrapper{
+  display: flex;
+  gap: 18px;
+}
+</style>
