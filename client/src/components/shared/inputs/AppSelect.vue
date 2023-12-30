4898a41cc5ea162ad765e68e0731ccf8b25ea2e5
christinanikolova <christinanikolova@github.com>
2023-12-30 14:59:09 +0200

    add AppSelect component

---
+++ client/src/components/shared/inputs/AppSelect.vue
@@ -0,0 +1,55 @@
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  label: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  errors: {
+    type: Array,
+    required: () => [],
+  },
+  categories: {
+    type: Array,
+    required: () => [],
+  },
+});
+const emit = defineEmits(['update:modelValue']);
+
+function onChange($event) {
+  const { value } = $event.target;
+  emit('update:modelValue', value);
+};
+</script>
+
+<template>
+  <div class="form-wrapper">
+    <label class="label label-small" :for="props.name">{{ props.label }}</label>
+    <div class="form-input-wrapper">
+      <select
+        :id="props.name"
+        class="input input-small"
+        :name="props.name"
+        :value="props.modelValue"
+        @change="onChange"
+      >
+        <option disabled value="">
+          Please relect {{ props.name }}
+        </option>
+        <option v-for="c in props.categories" :key="c.id ? c.id : c" :value="c.id ? c.id : c">
+          {{ c.name ? c.name : c }}
+        </option>
+      </select>
+      <ClientError v-for="error of props.errors" :key="error.$uid" :error="error.$message" />
+    </div>
+  </div>
+</template>
