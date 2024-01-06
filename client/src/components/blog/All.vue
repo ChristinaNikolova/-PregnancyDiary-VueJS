bfe81eada8246273506f35c064bfdc1547ad8946
christinanikolova <christinanikolova@github.com>
2024-01-06 10:39:05 +0200

    add html and css for the blog

--- client/src/components/blog/All.vue
+++ client/src/components/blog/All.vue
@@ -7,7 +7,7 @@ const articles = ref([]);
 onMounted(() => {
   articlesService
     .all()
-    .then(res => (articles.value = res))
+    .then(res => (articles.value = res.articles))
     .catch(err => console.error(err));
 });
 </script>
@@ -19,12 +19,101 @@ onMounted(() => {
       text="mommy-and-baby"
     />
     <div v-if="articles.length" class="blog-wrapper">
-      <h2>A</h2>
+      <h2 class="section-title">
+        Birth and Baby Blog
+      </h2>
+      <ul class="blog-ul">
+        <li v-for="a in articles" :key="a.id" class="blog-li">
+          <router-link :to="`blog/${a.id}`">
+            <img class="blog-li-picture" :src="a.picture">
+            <div class="blog-li-content-wrapper">
+              <h4 class="blog-li-title">
+                {{ a.title }}
+              </h4>
+              <span class="blog-li-content">{{ a.shortContent }}</span>
+            </div>
+          </router-link>
+        </li>
+      </ul>
     </div>
-    <Empty v-else element="articles" />
-    <Jumbo
-      image="/images/web3-woman-pregnant-light-spiritual-shutterstock_1466000780.webp"
-      text="pregnant-woman"
-    />
+    <template v-else>
+      <Empty element="articles" />
+      <Jumbo
+        image="/images/web3-woman-pregnant-light-spiritual-shutterstock_1466000780.webp"
+        text="pregnant-woman"
+      />
+    </template>
   </section>
 </template>
+
+<style scoped>
+.blog-wrapper {
+  text-align: center;
+  margin: 0 auto;
+  width: 90%;
+}
+
+.blog-ul {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 26px;
+  overflow: hidden;
+}
+
+.blog-li {
+  background-color: var(--clr-cream-bl);
+  padding: 0px 0px 40px 0px;
+}
+
+.blog-li-picture {
+  width: 100%;
+  object-fit: cover;
+}
+
+.blog-li:nth-child(3n+1) {
+  width: 100%;
+}
+
+.blog-li:nth-child(3n+1) .blog-li-picture {
+  height: 700px;
+}
+
+.blog-li:nth-child(3n+2),
+.blog-li:nth-child(3n+3) {
+  width: 49%;
+}
+
+.blog-li:nth-child(3n+2) .blog-li-picture,
+.blog-li:nth-child(3n+3) .blog-li-picture {
+  height: 70%;
+}
+
+.blog-li-content-wrapper {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.blog-li-title {
+  font-size: 24px;
+  font-weight: 400;
+  margin-bottom: 20px;
+  padding-top: 30px;
+}
+
+.blog-li-content {
+  line-height: 2;
+}
+
+.blog-li:hover {
+  cursor: pointer;
+}
+
+.blog-li:hover .blog-li-picture {
+  transform: scale(1.04);
+  transition: all .6s;
+}
+
+.blog-li:hover .blog-li-title {
+  text-decoration: underline;
+}
+</style>
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 import Blog from '../components/blog/All.vue';
+import SingleArticle from '../components/blog/Single.vue';
 import Login from '../components/auth/Login.vue';
 import Register from '../components/auth/Register.vue';
 import Logout from '../components/auth/Logout.vue';
@@ -8,10 +9,11 @@ import { isAdmin, isGuest, isUser } from '../guards/preGuards.js';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/blog', component: Blog },
   { path: '/login', component: Login, beforeEnter: isGuest },
   { path: '/register', component: Register, beforeEnter: isGuest },
   { path: '/logout', component: Logout, beforeEnter: isUser },
+  { path: '/blog', component: Blog },
+  { path: '/blog/:id', component: SingleArticle },
   { path: '/administration/category', component: () => import('../components/administration/category/All.vue'), beforeEnter: isAdmin },
   { path: '/administration/category/create', component: () => import('../components/administration/category/Create.vue'), beforeEnter: isAdmin },
   { path: '/administration/category/update/:id', component: () => import('../components/administration/category/Update.vue'), beforeEnter: isAdmin },
