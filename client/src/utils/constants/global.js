0cc1bbeb0b1302b7186489ca918fbc6d5d769ad5
christinanikolova <christinanikolova@github.com>
2024-01-20 10:27:45 +0200

    remove commented code

--- client/src/utils/constants/global.js
+++ client/src/utils/constants/global.js
@@ -27,37 +27,7 @@ export const queries = {
   BLOG_DEFAULT: '/blog?page=1&query=no%20search',
 };
 
-// export const classNames = {
-//   SHOW: 'show',
-//   HIDE: 'hide',
-//   FORM_WIDTH: 'form-width',
-//   CHEVRON_DOWN: 'fa-chevron-down',
-//   CHEVRON_RIGHT: 'fa-chevron-right',
-// };
-
-// export const styleNames = {
-//   FLEX: 'flex',
-//   NONE: 'none',
-//   INLINE_BLOCK: 'inline-block',
-//   BLOCK: 'block',
-// };
-
 // export const genders = {
 //   MALE: 'male',
 //   FEMALE: 'female',
-// };
-
-// export const addButtonTexts = {
-//   SUB_TASK: 'sub-task',
-//   TASK: 'task',
-//   EVENT: 'event',
-//   GUEST: 'guest',
-//   NOTE: 'note',
-//   COST: 'cost',
-//   COMMENT: 'comment',
-// };
-
-// export const tagNames = {
-//   P: 'P',
-//   H4: 'H4',
 // };
