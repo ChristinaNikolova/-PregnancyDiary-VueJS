02076fbfc3b48a5f4b8d2eb2e30750e574024725
christinanikolova <christinanikolova@github.com>
2024-01-27 10:44:11 +0200

    remove console.log

--- client/src/components/diary/Create.vue
+++ client/src/components/diary/Create.vue
@@ -14,7 +14,6 @@ const serverError = ref([]);
 const isDisabled = ref(true);
 
 function onSubmitHandler(positiveTestDate, dueDate, gender) {
-  console.log('in', positiveTestDate, dueDate, gender);
   diariesService
     .create(positiveTestDate, dueDate, gender)
     .then((res) => {
