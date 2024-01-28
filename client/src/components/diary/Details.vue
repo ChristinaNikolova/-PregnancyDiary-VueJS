9bb86e5a52f473cf17e63d0a2cd76b5a14a764c7
christinanikolova <christinanikolova@github.com>
2024-01-28 18:14:19 +0200

    working on css for details diary

--- client/src/components/diary/Details.vue
+++ client/src/components/diary/Details.vue
@@ -19,9 +19,116 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>Details</div>
+  <section class="diary-details">
+    <Jumbo
+      image="/images/personalised+pregnancy+journal.jpg"
+      text="diary"
+    />
+    <h2 class="section-title">
+      {{ diary.title }}
+    </h2>
+    <p class="diary-details-description">
+      {{ diary.description }}
+    </p>
+    <section class="diary-details-info">
+      <div class="diary-details-info-positive-test-date">
+        Positive Test: {{ diary.positiveTestDate }}
+      </div>
+      <div class="diary-details-info-due-date">
+        Due Date: {{ diary.dueDate }}
+      </div>
+      <div class="diary-details-info-gender">
+        Gender: <br>{{ diary.gender }}
+      </div>
+    </section>
+    <section class="diary-details-weeks">
+      <h3>Weeks</h3>
+      <div class="wrapper">
+        <h4>First Trimester</h4>
+        <ul>
+          <template v-for="w in diary.weeks">
+            <li v-if="w.trimester === 'first'" :key="w.id">
+              {{ w.title }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div class="wrapper">
+        <h4>Second Trimester</h4>
+        <ul>
+          <template v-for="w in diary.weeks">
+            <li v-if="w.trimester === 'second'" :key="w.id">
+              {{ w.title }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div class="wrapper">
+        <h4>Third Trimester</h4>
+        <ul>
+          <template v-for="w in diary.weeks">
+            <li v-if="w.trimester === 'third'" :key="w.id">
+              {{ w.title }}
+            </li>
+          </template>
+        </ul>
+      </div>
+    </section>
+  </section>
 </template>
 
 <style scoped>
+.diary-details {
+  background-color: var(--clr-light-brown);
+  color: var(--clr-white);
+}
 
+.diary-details .section-title {
+  color: var(--clr-white);
+}
+
+.diary-details-description {
+  text-align: center;
+  font-size: 18px;
+  text-transform: lowercase;
+  line-height: 2;
+  letter-spacing: 1.1px;
+  margin-bottom: 150px;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+
+.diary-details-info {
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 28px;
+  font-weight: 600;
+  color: var(--clr-brown);
+  margin-bottom: 200px;
+}
+
+.diary-details-info-positive-test-date,
+.diary-details-info-due-date,
+ .diary-details-info-gender {
+  width: 400px;
+  background-color: var(--clr-cream-bg);
+  padding: 100px;
+}
+
+.diary-details-info-positive-test-date {
+  border-radius: 70% 70% 70% 50% / 40% 60% 80% 80%;;
+}
+
+.diary-details-info-due-date {
+  border-radius: 30% 70% 20% 40% / 60% 40% 60% 40%;
+}
+
+.diary-details-info-gender {
+  border-radius: 70% 30% 30% 70% / 60% 40% 60% 40%;
+}
+
+.diary-details-weeks {
+  background-color: var(--clr-cream-bl);
+  color: var(--clr-dark-grey);
+}
 </style>
